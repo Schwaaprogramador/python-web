45d1a63b505a729741aac442d4447cb29d5ede11
Schwaaprogramador <danieltoro.programacion@gmail.com>
2023-09-23 14:01:03 -0500

    reflex config y venv

---
+++ NotasGenerales.py
@@ -0,0 +1,16 @@
+#-----------------------------------------
+#-----------------------------------------
+#La tecnologia para crear paginas web con python puro se llama REFLEX.
+#Pagina principal de REFLEX: https://reflex.dev/
+#Tutorial de MoureDev
+#https://www.youtube.com/watch?v=2u7JlBEavx0&list=PLH4-zYHtDkEjGJ1jRqB1yqgBNEoc0wnp2&index=6&t=888s&ab_channel=MoureDevbyBraisMoure
+
+
+#-----------------------------------------
+#-----------------------------------------
+#Lo mejor para trabajar con un proyecto de python es crear un ambiente virtual.
+#De esta forma se manejan mejor las dependencias.
+#Existen varias tecnologias para iniciar un ambiente:
+#poetry
+#venv => En la terminal: python -m venv venv // en mi computadora es: py -m venv nombre-del-dir
+#conda
